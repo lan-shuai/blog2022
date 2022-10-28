44d40d1783d8ee254c67cbf1f22e9b0640172799
lan-shuai <2812450388@qq.com>
2022-10-28 22:33:01 +0800

    feat: 调试react-router

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,14 +4,32 @@ import React, { memo, useEffect } from 'react';
 // import { useAppSelector, useAppDispatch } from 'modules/store';
 // import { selectGlobal, toggleSetting, toggleMenu, ELayout, switchTheme } from 'modules/global';
 // import Setting from './components/Setting';
-// import AppLayout from './components/AppLayout';
+import AppLayout from './components/AppLayout';
 // import Style from './index.module.less';
+
+
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+const Test1 = React.memo(() => {
+  return (
+    <div>Test1</div>
+  )
+})
+
+const Test2 = React.memo(() => {
+  return (
+    <div>Test2</div>
+  )
+})
+
+
 
 export default memo(() => {
   // const globalState = useAppSelector(selectGlobal);
   // const dispatch = useAppDispatch();
 
   // const AppContainer = AppLayout[globalState.isFullPage ? ELayout.fullPage : globalState.layout];
+  // const AppContainer = AppLayout
 
   // useEffect(() => {
   //   dispatch(switchTheme(globalState.theme));
@@ -43,7 +61,16 @@ export default memo(() => {
     //   </Drawer>
     // </Layout>
     <div>
-      layout
+      {/* <AppContainer/> */}
+
+      {/* <Test1/>
+      <Test2/> */}
+      <Routes>
+        {/* <Route path="/" element={<Test1/>} /> */}
+        <Route path="/test1" element={<Test1/>} ></Route>
+        <Route path="/test2" element={<Test2/>} ></Route>
+      </Routes>
+
     </div>
   );
 });
---
+++ src/pages/Overview/index.tsx
@@ -0,0 +1,20 @@
+import React, { memo, useState } from 'react';
+import classNames from 'classnames';
+import { useAppSelector } from 'modules/store';
+import { selectGlobal } from 'modules/global';
+
+import Style from './index.module.less';
+
+export default memo(() => {
+  const [type, setType] = useState('login');
+  const globalState = useAppSelector(selectGlobal);
+  const handleSwitchLoginType = () => {
+    setType(type === 'register' ? 'login' : 'register');
+  };
+
+  return (
+    <div>
+        Overview
+    </div>
+  );
+});
